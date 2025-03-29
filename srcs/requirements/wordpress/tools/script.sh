4d26ef300350bc95f87bc0cd533b0c2fac4015ea
Sacha Barthonet <sabartho@3D3.42angouleme.fr>
2025-03-29 07:07:23 +0100

    Fix some dumbs errors

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -7,7 +7,7 @@ chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb --allow-root
 ./wp-cli.phar core install --url=sabartho.42.fr --title=inception --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=admin@admin.com --allow-root
-./wp-cli.phar user create $DB_USER2 sabartho1@gmail.com --role=author --user_pass=$WP_PASS2 --allow-root
+./wp-cli.phar user create $WP_USER2 sabartho1@gmail.com --role=author --user_pass=$WP_PASS2 --allow-root
 
 ./wp-cli.phar plugin install redis-cache --activate --allow-root
 ./wp-cli.phar config set WP_REDIS_HOST redis --allow-root
