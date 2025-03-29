3c3cc0336ba1c08225b6470918b6daa68bf38b1d
Sacha Barthonet <sabartho@3A7.42angouleme.fr>
2025-03-29 23:30:00 +0100

    ⭐ END OF THE PROJECT

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -9,6 +9,8 @@ chmod +x wp-cli.phar
 ./wp-cli.phar core install --url=$DOMAIN_NAME --title=inception --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=$WP_MAIL --allow-root
 ./wp-cli.phar user create $WP_USER2 $WP_MAIL2 --role=author --user_pass=$WP_PASS2 --allow-root
 
+./wp-cli.phar theme install astra --activate --allow-root
+
 ./wp-cli.phar plugin install redis-cache --activate --allow-root
 ./wp-cli.phar config set WP_REDIS_HOST redis --allow-root
 ./wp-cli.phar config set WP_REDIS_PORT 6379 --allow-root
