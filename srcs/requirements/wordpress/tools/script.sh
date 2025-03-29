3fdbd2da1323c473db6082bc3dd209216f511baa
Sacha Barthonet <sabartho@3A7.42angouleme.fr>
2025-03-29 20:53:23 +0100

    ADD more env variables for more visibility and security

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -5,9 +5,9 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=sabartho.42.fr --title=inception --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=admin@admin.com --allow-root
-./wp-cli.phar user create $WP_USER2 sabartho1@gmail.com --role=author --user_pass=$WP_PASS2 --allow-root
+./wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb --allow-root
+./wp-cli.phar core install --url=$DOMAIN_NAME --title=inception --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=$WP_MAIL --allow-root
+./wp-cli.phar user create $WP_USER2 $WP_MAIL2 --role=author --user_pass=$WP_PASS2 --allow-root
 
 ./wp-cli.phar plugin install redis-cache --activate --allow-root
 ./wp-cli.phar config set WP_REDIS_HOST redis --allow-root
