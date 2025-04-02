9df3a176c42d01a47d382a46f1850e655f32e339
Sacha Barthonet <sabartho@3D2.42angouleme.fr>
2025-04-02 20:47:35 +0200

    ADD editor permission for user 2

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -8,6 +8,7 @@ chmod +x wp-cli.phar
 ./wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb --allow-root
 ./wp-cli.phar core install --url=$DOMAIN_NAME --title=inception --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=$WP_MAIL --allow-root
 ./wp-cli.phar user create $WP_USER2 $WP_MAIL2 --role=author --user_pass=$WP_PASS2 --allow-root
+./wp-cli.phar user set-role 2 editor --allow-root
 
 ./wp-cli.phar theme install astra --activate --allow-root
 
